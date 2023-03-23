6b9566d47149cae34fbf2ed4eae38c1dc2292ce9
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-03-23 04:58:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo rm -rf /etc/apt/sources.list
 sudo rm -rf /etc/apt/sources.list.d/brave-browser-release.list
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-sudo wget -O https://github.com/SteavenBuntu/SteavenRepos/raw/main/sources.list /etc/apt/sources.list
-sudo wget -O https://github.com/SteavenBuntu/SteavenRepos/raw/main/sources.list.d/brave-browser-release.list /etc/apt/sources.list.d/brave-browser-release.list
+sudo wget -O /etc/apt/sources.list https://github.com/SteavenBuntu/SteavenRepos/raw/main/sources.list
+sudo wget -O /etc/apt/sources.list.d/brave-browser-release.list https://github.com/SteavenBuntu/SteavenRepos/raw/main/sources.list.d/brave-browser-release.list
